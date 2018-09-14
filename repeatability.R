1da9183b962be9d8a6065796b0e062bf7330c3a3
thewart <seth.madlonkay@gmail.com>
2018-09-14 18:54:56 -0400

    minor tweaks

--- repeatability.R
+++ repeatability.R
@@ -2,8 +2,12 @@ X <- model.matrix( ~ 0 + FocalID,data=used_obs) %>% as.matrix() %>% t()
 lp0 <- extract(fit0,"lp")$lp %>% colMeans()
 lp_mm <- extract(fit_mm,"lp")$lp %>% colMeans()
 lp_f1 <- extract(fit_mm_f1,"lp")$lp %>% colMeans()
+lp_f2 <- extract(fit_mm_f2,"lp")$lp %>% colMeans()
 
-Rmm <- -(X %*% (lp_mm-lp0)) / (X %*% lp0)
-Rf1 <- -(X %*% (lp_f1-lp0)) / (X %*% lp0)
-Rdiff <- (Rmm-Rf1)/Rmm
-Rdiff[Rdiff<0] <- 0 #hack for correcting monte-carlo error
+
+Rmm <- (X %*% (lp_mm-lp0)) / rowSums(X)
+Rf1 <- (X %*% (lp_f1-lp0)) / rowSums(X)
+R0 <- (X %*% -lp0) / rowSums(X)
+Rdiff <- (X %*% (lp_mm-lp_f1)) / rowSums(X)
+RDR <- Rdiff/Rmm 
+RDR[RDR<0] <- 0 #hack for correcting monte-carlo error
