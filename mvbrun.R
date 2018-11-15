23acdc86bc36000ee18dea5904283de4b9af101e
thewart <seth.madlonkay@gmail.com>
2018-11-15 01:31:37 -0500

    bug fixes/cleanup

--- mvbrun.R
+++ mvbrun.R
@@ -10,7 +10,7 @@ source("~/code/OrdRegMix/loadcovariates.R")
 source("~/code/OrdRegMix/tabulatefocaldata.R")
 source("~/code/OrdRegMix/preparemodel.R")
 
-std <- standardize(as.formula(paste0("SDB ~ ",leftside)),cov_dat,family=binomial)
+std <- standardize(as.formula(paste0("Age>0 ~ ",leftside)),cov_dat,family=binomial)
 
 Xf <- model.matrix(lm(formula=std$formula,data=std$data))[,-1]
 Xr <- model.matrix( ~ 0 + FocalID,data=cov_dat) %>% as.matrix()
@@ -22,15 +22,15 @@ mvb_mm <- stan_model("~/code/MultVarBinom/mvbinom_mm.stan")
 mvb_mm_f1 <- stan_model("~/code/MultVarBinom/mvbinom_mm_f1.stan")
 # mvb_mm_f2 <- stan_model("~/code/MultVarBinom/mvbinom_mm_f2.stan")
 
-standat <- list(Y=t(Y),N=nrow(Y),D=ncol(Y),M=length(n),n=n,D2=2^nrow(Y),X=t(Xf),P=ncol(Xf))
+standat <- list(Y=t(Y),N=nrow(Y),D=ncol(Y),M=length(n),n=n,D2=2^ncol(Y),X=t(Xf),P=ncol(Xf))
 
 fit0 <- sampling(mvb,standat,
-                 iter=534,warmup=200,thin=2,chains=3,cores=3)
+                 iter=1000,warmup=250,thin=3,chains=4,cores=4)
 
 standat <- c(standat,list(Z=t(Xr),R=nZ(Xr)))
 # fit_mm_f2 <- sampling(mvb_mm_f2,standat,pars=c("f2_u_raw","f2_sigma_raw"),
 #                       iter=700,warmup=200,thin=2,chains=4,include=F,cores=4)
-fit_mm_f1 <- sampling(mvb_mm_f1,standat,pars=c("f1_u_raw"),
-                      iter=534,warmup=200,thin=2,chains=3,include=F,cores=3)
-fit_mm <- sampling(mvb_mm,standat,pars=c("f1_u_raw","f2_u_raw","f2_sigma_raw"),
-                   iter=534,warmup=200,thin=2,chains=3,include=F,cores=3)
+fit_mm_f1 <- sampling(mvb_mm_f1,standat,pars=c("f1_u_raw"),include=F,
+                      iter=1000,warmup=250,thin=3,chains=4,cores=4)
+fit_mm <- sampling(mvb_mm,standat,pars=c("f1_u_raw","f2_u_raw","f2_sigma_raw"),include=F,
+                   iter=1000,warmup=250,thin=3,chains=4,cores=4)
