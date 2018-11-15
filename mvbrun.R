2731837789a2587440e4ff120833e178a47b109f
thewart <seth.madlonkay@gmail.com>
2018-11-14 20:04:37 -0500

    compat w new data and model specification pipeline

--- mvbrun.R
+++ mvbrun.R
@@ -8,27 +8,12 @@ modonrank <- F
 source("~/code/OrdRegMix/collectcleanfocaldata.R")
 source("~/code/OrdRegMix/loadcovariates.R")
 source("~/code/OrdRegMix/tabulatefocaldata.R")
+source("~/code/OrdRegMix/preparemodel.R")
 
-# behaviors = c("SDB","GroomGIVE", "GroomGET","passcont","Approach:initiate(focal)",
-#               "Approach:initiate(partner)","NonConAgg_give","NonConAgg_rec",
-#               "contactAgg:direct'n(give)","contactAgg:direct'n(receive)")
+std <- standardize(as.formula(paste0("SDB ~ ",leftside)),cov_dat,family=binomial)
 
-behaviors <- c("SDB","Agg_kin","Agg_nonkin",
-               "GroomGIVE:samesex(TRUE):iskin(TRUE)",
-               "GroomGIVE:samesex(TRUE):iskin(FALSE)",
-               "passcont:samesex(TRUE):iskin(TRUE)",
-               "passcont:samesex(TRUE):iskin(FALSE)",
-               "Approach:initiate(focal):samesex(TRUE):iskin(TRUE)",
-               "Approach:initiate(focal):samesex(TRUE):iskin(FALSE)")
-
-used_obs <- all_obs[Group=="F" & paste0(FocalID,Year) %in% id_dat[SEX=="f" & kin_samesex>min(kin_samesex),paste0(FocalID,Year)]]
-cov_dat <- id_dat[SEX=="f" & Group=="F" & kin_samesex>min(kin_samesex)]
-
-std <- standardize(rep(1,nrow(cov_dat)) ~ Year + kin_samesex,cov_dat,family=binomial)
-
-Xf <- model.matrix(lm(formula=std$formula,data=std$data))[,-1] %>% t()
-Xr <- model.matrix( ~ 0 + FocalID,data=cov_dat) %>% as.matrix() %>% t()
-Y <- used_obs[,behaviors,with=F] %>% as.matrix() %>% t()
+Xf <- model.matrix(lm(formula=std$formula,data=std$data))[,-1]
+Xr <- model.matrix( ~ 0 + FocalID,data=cov_dat) %>% as.matrix()
 n <- used_obs[,length(Observation),by=.(FocalID,Year)]$V1
 M <- length(n)
 
@@ -37,15 +22,15 @@ mvb_mm <- stan_model("~/code/MultVarBinom/mvbinom_mm.stan")
 mvb_mm_f1 <- stan_model("~/code/MultVarBinom/mvbinom_mm_f1.stan")
 # mvb_mm_f2 <- stan_model("~/code/MultVarBinom/mvbinom_mm_f2.stan")
 
-standat <- list(Y=Y,N=ncol(Y),D=nrow(Y),M=length(n),n=n,D2=2^nrow(Y),X=Xf,P=nrow(Xf))
+standat <- list(Y=t(Y),N=nrow(Y),D=ncol(Y),M=length(n),n=n,D2=2^nrow(Y),X=t(Xf),P=ncol(Xf))
 
 fit0 <- sampling(mvb,standat,
-                 iter=700,warmup=200,thin=2,chains=4,cores=4)
+                 iter=534,warmup=200,thin=2,chains=3,cores=3)
 
-standat <- c(standat,list(Z=Xr,R=nrow(Xr)))
+standat <- c(standat,list(Z=t(Xr),R=nZ(Xr)))
 # fit_mm_f2 <- sampling(mvb_mm_f2,standat,pars=c("f2_u_raw","f2_sigma_raw"),
 #                       iter=700,warmup=200,thin=2,chains=4,include=F,cores=4)
 fit_mm_f1 <- sampling(mvb_mm_f1,standat,pars=c("f1_u_raw"),
-                      iter=700,warmup=200,thin=2,chains=4,include=F,cores=4)
+                      iter=534,warmup=200,thin=2,chains=3,include=F,cores=3)
 fit_mm <- sampling(mvb_mm,standat,pars=c("f1_u_raw","f2_u_raw","f2_sigma_raw"),
-                   iter=700,warmup=200,thin=2,chains=4,include=F,cores=4)
+                   iter=534,warmup=200,thin=2,chains=3,include=F,cores=3)
